c7494d8c17006067f9768beeaae10e5db1fb67f5
bsantanas <bsantana@hellobundle.com>
2015-05-30 03:16:21 +0100

    Initial Commit

---
+++ SafeMatch/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SafeMatch
+//
+//  Created by Bernardo Santana on 5/30/15.
+//  Copyright (c) 2015 BUM. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SafeMatchTests/SafeMatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  SafeMatchTests.swift
+//  SafeMatchTests
+//
+//  Created by Bernardo Santana on 5/30/15.
+//  Copyright (c) 2015 BUM. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class SafeMatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
